88f96becb56c8c8fc5a586c91cd4ec4178db9045
Christopher Lind <Christopher.lind.c@gmail.com>
2020-12-09 18:42:32 +0100

    Cleaned up Instruction class - added comments Renamed Dec8Test to Dec8Refactored Cleaned up Dec8Refactored - added comments

--- Dec8Refactored.cs
+++ Dec8Refactored.cs
@@ -4,18 +4,18 @@ using System.Text;
 
 namespace AdventOfCode
 {
-    class Dec8Test
+    class Dec8Refactored
     {
         List<string> dataList;
         List<Instruction> allInstructions = new List<Instruction>();
 
-        public Dec8Test()
+        public Dec8Refactored()
         {
             dataList = ReadDataFile.FileToListSimple("AdventCode8Dec.txt");
             CreateInstructions();
         }
 
-        public Dec8Test(List<string> testList)
+        public Dec8Refactored(List<string> testList)
         {
             dataList = testList;
             CreateInstructions();
@@ -26,6 +26,7 @@ namespace AdventOfCode
             allInstructions = InstructionMethods.CreateVM(dataList);
         }
 
+        // Finds total of accumulator at the start of the infinite loop
         public int SolutionPart1()
         {
             List<Instruction> visistedInstr = new List<Instruction>();
@@ -42,59 +43,48 @@ namespace AdventOfCode
             return accumulator;
         }
 
-        //public void testCount()
-        //{
-        //    Dictionary<int, int> counter = new Dictionary<int, int>();
-        //    int key;
-        //    foreach (Instruction instr in allInstructions)
-        //    {
-        //        key = instr.FormerIndices.Count;
-        //        if (counter.ContainsKey(key))
-        //            counter[key]++;
-        //        else
-        //            counter.Add(key, 1);
-        //    }
-
-        //    foreach (int intKey in counter.Keys)
-        //    {
-        //        Console.WriteLine($"Former Indices {intKey} - Count: {counter[intKey]}");
-        //    }
-        //}
-
-
+        // Flips exactly one(1) instruction from Nop->Jmp or Jmp->Nop so the program can run all the way through.
+        // Prints the accumulator after that.
         public int SolutionPart2()
         {
+            // list of all indices of Instructions that can be reached if starting from the last instruction and working backwards
             List<int> vmReversed = InstructionMethods.BackTravelFromIndex(allInstructions, allInstructions.Count - 1);
 
             int accumulator = 0;
+            // VM should run from the top
             int currentIndex = 0;
-            int tempIndex;
+            // Current instruction in the loop and a tempInstruction used for switching
             Instruction currInstr;
             Instruction tempInstr = null;
 
+            // if the switch is found, no reason to test anymore
             bool switchFound = false;
+            
+            // Program should stop when the index reaches the end
             while (currentIndex != allInstructions.Count)
             {
                 currInstr = allInstructions[currentIndex];
-                tempIndex = currentIndex;
 
+                // test for Instruction type and makes a temp swapped version of it.
                 if (!switchFound && currInstr is Nop)
                     tempInstr = InstructionMethods.SwapType<Jmp>(currInstr);
                 else if (!switchFound && currInstr is Jmp)
                     tempInstr = InstructionMethods.SwapType<Nop>(currInstr);
 
+                // if the next index of the swapped instruction is in the vmreversed list - 
+                // then the switch will cause the program to complete
                 if (tempInstr != null && vmReversed.Contains(tempInstr.NextIndex))
                 {
+                    // current instruction is set to the swapped
                     currInstr = tempInstr;
                     switchFound = true;
                 }
+                // current instruction runs its action against index and accumulator
                 currInstr.Action(ref currentIndex, ref accumulator);
+                // the temp instruction is removed
                 tempInstr = null;
             }
-            
             return accumulator;
         }
-
-        
     }
 }
--- Instruction.cs
+++ Instruction.cs
@@ -6,28 +6,37 @@ namespace AdventOfCode
 {
     public static class InstructionMethods
     {
+        //
         // creates a list of instruction based on the raw VM
         public static List<Instruction> CreateVM(List<string> rawInstructions)
         {
             List<Instruction> instructions = new List<Instruction>();
+            
             // creates list of basic Instructions
             for (int i = 0; i < rawInstructions.Count; i++)
             {
                 instructions.Add(CreateInstruction(rawInstructions[i], i));
             }
 
-            UpdateFormerIndices(ref instructions);
+            // finds former indices for all instructions
+            UpdateFormerIndices(ref instructions, false);
             
             return instructions;
         }
-
-        public static void UpdateFormerIndices(ref List<Instruction> instructions)
+        
+        //
+        // Updates the list of indices that can lead directly to an instruction.
+        public static void UpdateFormerIndices(ref List<Instruction> instructions, bool reset = true)
         {
-            // resets all former indices lists.
-            foreach (Instruction instruction in instructions)
+            if (reset)
             {
-                instruction.FormerIndices = new List<int>();
+                // resets all former indices lists.
+                foreach (Instruction instruction in instructions)
+                {
+                    instruction.FormerIndices = new List<int>();
+                }
             }
+
 
             // updates the new list
             Instruction tempInstr;
@@ -40,14 +49,16 @@ namespace AdventOfCode
                 }
             }
         }
-
-        // Creates a single instruction based on the VM
+        
+        //
+        // Creates a single instruction based on a line of VM string
         private static Instruction CreateInstruction(string rawInstruction, int index)
         {
             string[] splitData = rawInstruction.Split(' ');
             string instrName = splitData[0];
             int argument = Int32.Parse(splitData[1]);
 
+            //switch case easily expandable
             switch (instrName)
             {
                 case "acc":
@@ -60,14 +71,19 @@ namespace AdventOfCode
                     return new Nop(argument, index);
             }
         }
-
+        
+        //
         // From a List of Instructions and a Index, travels backwards and makes a list of all instructions visited that way.
         public static List<int> BackTravelFromIndex(List<Instruction> instructions, int fromIndex)
         {
+            // Uses Graph traversal logic - the list of instructions never change.
             List<int> visitedInstructions = new List<int>();
             Instruction currInstr = instructions[fromIndex];
 
-            visitedInstructions.Add(currInstr.Index);            
+            // always add current index to the list
+            visitedInstructions.Add(currInstr.Index);
+            
+            // returns from here, if no instructions leads to the current instruction
             if (currInstr.FormerIndices.Count == 0)
                 return visitedInstructions;
             
@@ -77,7 +93,8 @@ namespace AdventOfCode
             }
             return visitedInstructions;
         }
-
+        
+        //
         // Swaps the type of instruction
         public static Instruction SwapType<T>(Instruction instruction) where T : Instruction
         {
@@ -87,35 +104,24 @@ namespace AdventOfCode
         
     }
 
-
+    // The abstract instruction class - all Instructions will be derived from this.
+    // Instruction itself is not callable.
     public abstract class Instruction
     {
         // Index of the instruction in the VM and the next index and the one that comes before
         private int _index;
-        
-        // The next Index, and the Index that points here.
-        private Instruction _nextInstruct = null;
-        private Instruction _formerInstruct = null;
 
+        // List of Indexes that leads directly to this instruction
         private List<int> _formerIndices = new List<int>();
 
         // The instruction data
         private int _argument;
-        
-        // Is it the original type
-        private bool _origInstruct = true;
 
-        // If the instruction have been switched - this is the original
-        private Instruction _oldInstruct = null;
-        //private  _oldInstruct = null;
-
-        public int Argument { get => _argument; }
-        public bool OrigInstruct { get => _origInstruct; }
-        public int Index { get => _index; }
-        internal Instruction OldType { get => _oldInstruct; }
-        public Instruction NextInstruct { get => _nextInstruct; set => _nextInstruct = value; }
-        public Instruction FormerInstruct { get => _formerInstruct; set => _formerInstruct = value; }
+        // The calculated index that follows this instruction
         public int NextIndex { get => GetNextIndex(); }
+        
+        public int Argument { get => _argument; }
+        public int Index { get => _index; }
         public List<int> FormerIndices{ get => _formerIndices; set => _formerIndices = value; }
 
         public Instruction(int argument, int index)
@@ -123,38 +129,42 @@ namespace AdventOfCode
             _index = index;
             _argument = argument;
         }
-
-        // Overload method used if switching from one instruction to another
+        
+        //
+        // Overload method used if switching from one type of instruction to another
         public Instruction(Instruction instruction)
         {
             _index = instruction.Index;
             _argument = instruction.Argument;
-            _origInstruct = !instruction.OrigInstruct;
-            _oldInstruct = instruction;
         }
 
+        // What action should be peformed by the instruction
         public abstract void Action(ref int index, ref int accumulator);
+        // if the action needs to be reverted
         public abstract void RevertAction(ref int index, ref int accumulator);
 
+        // finds the next index in the vm, by running it's action against its own index
         public int GetNextIndex()
         {
             int thrownInt = 0;
-            int nextIndex = this.Index;
+            int nextIndex = Index;
             Action(ref nextIndex, ref thrownInt);
             return nextIndex;
         }
-
     }
-
+    
+    //
+    // acc class - adds it's argument to accumulator and goes to next instruction
     public class Acc : Instruction
     {
         public Acc(int argument, int index) : base(argument, index)
         {
         }
-
         public Acc(Instruction instruction) : base(instruction)
         {
         }
+
+        // Accs increase the index and adds its argument to the accumelator
         public override void Action(ref int index, ref int accumelator)
         {
             index++;
@@ -168,49 +178,38 @@ namespace AdventOfCode
         }
     }
 
+    //
+    // Jmp class - jumps the next instruction by adding its argument to its index
     public class Jmp : Instruction
     {
         public Jmp(int argument, int index) : base(argument, index)
         {
-
         }
-
         public Jmp(Instruction instruction) : base(instruction)
         {
-
         }
 
-        public override void Action(ref int index, ref int accumulator)
-        {
-            index += Argument;
-        }
+        // jmps increase the index by the instructions argument
+        public override void Action(ref int index, ref int accumulator) => index += Argument;
 
-        public override void RevertAction(ref int index, ref int accumulator)
-        {
-            index -= Argument;
-        }
+        public override void RevertAction(ref int index, ref int accumulator) => index -= Argument;
+
     }
 
+    //
+    // Nop class - goes to next instruction
     public class Nop : Instruction
     {
         public Nop(int argument, int index) : base(argument, index)
         {
-
         }
-
         public Nop(Instruction instruction) : base(instruction)
         {
-
         }
 
-        public override void Action(ref int index, ref int accumulator)
-        {
-            index++;
-        }
+        // Nops only action is increase the index by one
+        public override void Action(ref int index, ref int accumulator) => index++;
 
-        public override void RevertAction(ref int index, ref int accumulator)
-        {
-            index--;
-        }
+        public override void RevertAction(ref int index, ref int accumulator) => index--;
     }
 }
