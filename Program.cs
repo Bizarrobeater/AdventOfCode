5f01c4181d06b3efa5b90d804f9037d5b1f900fd
Christopher Lind <Christopher.lind.c@gmail.com>
2020-12-13 16:23:34 +0100

    New Derived based with advanced testing Solution to dec 12 part 1 and 2

--- Program.cs
+++ Program.cs
@@ -12,24 +12,33 @@ namespace AdventOfCode
             //WithTestDataPart1();
             //WithTestDataPart2();
 
+            //TestManyCases();
 
-            var test = new Dec12();
+
+            var test = new Dec13();
             //test.Timer("Solution1", test.Solution1);
             test.Timer("Solution2", test.Solution2);
         }
 
         static void WithTestDataPart1()
         {
-            Dec12 test = new Dec12(TestData.Dec12Part1);
+            var test = new Dec13(TestData.Dec13Part1);
             Console.WriteLine(test.Solution1());
             //test.test();
         }
 
         static void WithTestDataPart2()
         {
-            Dec12 test = new Dec12(TestData.Dec12Part1);
+            var test = new Dec12(TestData.Dec12Part1);
             Console.WriteLine(test.Solution2());
             //test.test();
+        }
+
+        static void TestManyCases()
+        {
+            var test = new Dec13(TestData.Dec13Part2);
+            test.TestAllCases(test.Solution2);
+
         }
 
 
